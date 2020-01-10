70c70f3bf7fda65b8003cb649ca84574f0b1360f
alencion <rhqnrdl625@naver.com>
2020-01-10 12:35:39 -0500

    init

---
+++ LoRa/LoRa_Client_skeleton/LoRa_Client_skeleton.ino
@@ -0,0 +1,54 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+float frequency = 915.0;
+
+void setup() 
+{
+  Serial.begin(9600);
+  //while (!Serial) ; // Wait for serial port to be available
+  Serial.println("Start LoRa Client");
+  if (!rf95.init())
+    Serial.println("init failed");
+  // Setup ISM frequency
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(13);
+
+}
+
+void loop()
+{
+  Serial.println("Sending to LoRa Server");
+  // Send a message to LoRa Server
+  uint8_t data[] = "Hello, this is device 1";
+  rf95.send(data, sizeof(data));
+  
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (rf95.waitAvailableTimeout(3000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is LoRa server running?");
+  }
+  delay(5000);
+}
